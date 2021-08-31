eb722f8aea5df7271d8751b969ff3e3f727ddd4b
Dmitriy Mihaylenko <dm.mihaylenko@gmail.com>
2021-08-31 10:46:41 +0300

    add go back to game page, fix script loading

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -1,14 +1,30 @@
 import React, { useEffect } from "react";
+import { Button, Icon } from "semantic-ui-react";
 import { useParams } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import Header from "../shared/Header/Header";
+import { userState } from "../../common/States";
 
 export default function GamePage() {
   let { gameCode } = useParams();
 
+  const history = useHistory();
+
+  const [user, setUser] = useRecoilState(userState);
+
   useEffect(() => {
-    window.comeon.game.launch(gameCode);
+    if (user) {
+      window.comeon.game.launch(gameCode);
+    } else {
+      history.push("/login");
+    }
   }, []);
+
+  const onBackClick = () => {
+    history.push("/");
+  };
 
   return (
     <>
@@ -18,6 +34,10 @@ export default function GamePage() {
         <Grid.Row centered className="padding-0">
           <Grid.Column width={12} className="bg-white padding-tb20">
             <div id="game-launch" className="full-width flex-row justify-content-center"></div>
+            <Button secondary onClick={onBackClick}>
+              <Icon name="chevron left" />
+              Go back
+            </Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
